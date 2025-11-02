c95ad691841f215d55b086a04a627f93b8c5a1f9
eyoon <euiseonyoon@gmail.com>
2025-11-02 12:28:28 +0900

    init

---
+++ src/main/kotlin/com/luke/kHelperServer/KHelpServerApplication.kt
@@ -0,0 +1,11 @@
+package com.luke.kHelperServer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KHelpServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KHelpServerApplication>(*args)
+}
