60ea2f5c88ae68df2edab9b08727ccaf9529175c
eyoon <euiseonyoon@gmail.com>
2025-11-03 18:18:11 +0900

    feat: @EnableJpaRepositories, @EnableMongoRepositories 추가

--- src/main/kotlin/com/luke/kHelperServer/KHelpServerApplication.kt
+++ src/main/kotlin/com/luke/kHelperServer/KHelpServerApplication.kt
@@ -2,8 +2,12 @@ package com.luke.kHelperServer
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = ["com.luke.kHelperServer.adapter.secondary.db"])
+@EnableMongoRepositories(basePackages = ["com.luke.kHelperServer.adapter.secondary.db"])
 class KHelpServerApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/luke/kHelperServer/configuration/MongoConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.luke.kHelperServer.configuration
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
-
-@Configuration
-@EnableMongoRepositories(basePackages = ["com.luke.kHelperServer.domain"])
-class MongoConfig
