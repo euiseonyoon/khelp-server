89e958c2a4349abdcf92d7a88ebaabed58d7a4be
eyoon <euiseonyoon@gmail.com>
2025-11-04 18:26:06 +0900

    feat: GlobalExceptionHandler(@RestControllerAdvice) 구현

---
+++ src/main/kotlin/com/luke/kHelperServer/adapter/primary/webapi/common/GlobalExceptionHandler.kt
@@ -0,0 +1,28 @@
+package com.luke.kHelperServer.adapter.primary.webapi.common
+
+import com.luke.kHelperServer.common.GlobalResponse
+import com.luke.kHelperServer.domain.exception.BizException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+
+    @ExceptionHandler(BizException::class)
+    fun handleBizException(e: BizException): ResponseEntity<GlobalResponse<Nothing>> {
+        return GlobalResponse.createBizExceptionResponse(e, HttpStatus.BAD_REQUEST)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleException(e: Exception): ResponseEntity<GlobalResponse<Nothing>> {
+        val response = GlobalResponse(
+            isError = true,
+            result = null,
+            errorMsg = "An unexpected error occurred.",
+            errorCode = "INTERNAL_SERVER_ERROR",
+        )
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response)
+    }
+}
