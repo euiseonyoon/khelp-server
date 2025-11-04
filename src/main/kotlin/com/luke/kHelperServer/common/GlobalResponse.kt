af1f9bbed5c2dd4a8c8c5385a2f2af72797eaa8b
eyoon <euiseonyoon@gmail.com>
2025-11-04 17:23:38 +0900

    feat: GlobalResponse 정의

---
+++ src/main/kotlin/com/luke/kHelperServer/common/GlobalResponse.kt
@@ -0,0 +1,36 @@
+package com.luke.kHelperServer.common
+
+import com.luke.kHelperServer.domain.exception.BizException
+import kotlinx.serialization.Serializable
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+
+@Serializable
+data class GlobalResponse<T>(
+    val isError: Boolean,
+    val result: T?,
+    val errorMsg: String?,
+    val errorCode: String?,
+) {
+    companion object {
+        fun <T> create(result: T): GlobalResponse<T> {
+            return GlobalResponse(false, result, null, null)
+        }
+
+        fun createFromBizException(bizException: BizException): GlobalResponse<Nothing> {
+            return GlobalResponse(true, null, bizException.message, bizException.code)
+        }
+
+        fun <T> createResponse(result: T): ResponseEntity<GlobalResponse<T>> {
+            return ResponseEntity.ok(this.create(result))
+        }
+
+        fun createBizExceptionResponse(
+            bizException: BizException,
+            httpStatus: HttpStatus,
+        ): ResponseEntity<GlobalResponse<Nothing>>{
+            require(httpStatus.isError)
+            return ResponseEntity.status(httpStatus.value()).body(this.createFromBizException(bizException))
+        }
+    }
+}
